344c308bc34f344d79dd6847e442b1d8cb910ac9
ArtiomKochkin <artiom.kochkin@yandex.ru>
2023-08-14 23:00:16 +0700

    add a password generation function

--- src/js/operations/password.js
+++ src/js/operations/password.js
@@ -9,148 +9,98 @@ export function password() {
     const btnGeneration = document.getElementById("btnGeneration");
     const resultBlock = document.querySelector(".result");
     const passwordResult = document.getElementById("passwordResult");
-
+    
     let engUpperLang = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     let engLowerLang = "abcdefghijklmnopqrstuvwxyz";
     let rusUpperLang = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
     let rusLowerLang = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
-    let otherCharacters = "%*()?!@#~";
+    let otherCharacters = "%*()?!@#~-+/$&=<>_";
     let numberCharacters = "0123456789";
     let resultChar;
     
     btnGeneration.addEventListener("click", createPassword);    
-
+    
     function createPassword() {
 
-        function generateNumber() {
-            resultChar = Math.floor(Math.random() * 10);
+      function generateChar(lengthArray) {
+        let characters = "";
+        for (let i = 1; i < arguments.length; i++) {
+            characters += arguments[i];
         }
-        
-        function generateOtherChar() {
-            let random = Math.floor(Math.random() * 8);
-            resultChar = otherCharacters[random];
-        }
+        let random = Math.floor(Math.random() * lengthArray);
+        resultChar = characters[random];
+      }
 
-        function generateEngLowerChar() {
-            let random = Math.floor(Math.random() * 25);
-            resultChar = engLowerLang[random];
+      function checkSettings() {
+        if (upperChars.checked == true && engChars.checked == true && specialChars.checked == true && numbers.checked == true && lowerChars.checked == true) {
+            generateChar(78, numberCharacters, engLowerLang, engUpperLang, otherCharacters);
+        } else if (upperChars.checked == true && rusChars.checked == true && specialChars.checked == true && numbers.checked == true && lowerChars.checked == true) {
+            generateChar(92, numberCharacters, rusLowerLang, rusUpperLang, otherCharacters);
+        } else if (upperChars.checked == true && engChars.checked == true && specialChars.checked == true && numbers.checked == true) {
+            generateChar(53, numberCharacters, engUpperLang, otherCharacters);
+        } else if (lowerChars.checked == true && engChars.checked == true && specialChars.checked == true && numbers.checked == true) {
+            generateChar(53, numberCharacters, engLowerLang, otherCharacters);
+        } else if (upperChars.checked == true && rusChars.checked == true && specialChars.checked == true && numbers.checked == true) {
+            generateChar(60, numberCharacters, rusUpperLang, otherCharacters);
+        } else if (lowerChars.checked == true && rusChars.checked == true && specialChars.checked == true && numbers.checked == true) {
+            generateChar(60, numberCharacters, rusLowerLang, otherCharacters);
+        } else if (upperChars.checked == true && engChars.checked == true && lowerChars.checked == true && numbers.checked == true) {
+            generateChar(60, engLowerLang, engUpperLang, numberCharacters);
+        } else if (upperChars.checked == true && rusChars.checked == true && lowerChars.checked == true && numbers.checked == true) {
+            generateChar(74, rusLowerLang, rusUpperLang, numberCharacters);
+        } else if (upperChars.checked == true && engChars.checked == true && lowerChars.checked == true && specialChars.checked == true) {
+            generateChar(68, engLowerLang, engUpperLang, otherCharacters);
+        } else if (upperChars.checked == true && rusChars.checked == true && lowerChars.checked == true && specialChars.checked == true) {
+            generateChar(82, rusLowerLang, rusUpperLang, otherCharacters);
+        } else if (upperChars.checked == true && engChars.checked == true && lowerChars.checked == true) {
+            generateChar(50, engLowerLang, engUpperLang);
+        } else if (upperChars.checked == true && rusChars.checked == true && lowerChars.checked == true) {
+            generateChar(64, rusLowerLang, rusUpperLang);
+        } else if (upperChars.checked == true && engChars.checked == true && numbers.checked == true) {
+            generateChar(35, numberCharacters, engUpperLang);
+        } else if (lowerChars.checked == true && engChars.checked == true && numbers.checked == true) {
+            generateChar(35, numberCharacters, engLowerLang);
+        } else if (lowerChars.checked == true && rusChars.checked == true && numbers.checked == true) {
+            generateChar(42, numberCharacters, rusLowerLang);
+        } else if (upperChars.checked == true && rusChars.checked == true && numbers.checked == true) {
+            generateChar(42, numberCharacters, rusUpperLang);
+        } else if (upperChars.checked == true && engChars.checked == true && specialChars.checked == true) {
+            generateChar(43, otherCharacters, engUpperLang);
+        } else if (lowerChars.checked == true && engChars.checked == true && specialChars.checked == true) {
+            generateChar(43, otherCharacters, engLowerLang);
+        } else if (lowerChars.checked == true && rusChars.checked == true && specialChars.checked == true) {
+            generateChar(50, otherCharacters, rusLowerLang);
+        } else if (upperChars.checked == true && rusChars.checked == true && specialChars.checked == true) {
+            generateChar(50, otherCharacters, rusUpperLang);
+        } else if (specialChars.checked == true && numbers.checked == true) { 
+            generateChar(28, otherCharacters, numberCharacters);
+        } else if (upperChars.checked == true && engChars.checked == true) {
+            generateChar(25, engUpperLang);
+        } else if (upperChars.checked == true && rusChars.checked == true) {
+            generateChar(32, rusUpperLang);
+        } else if (lowerChars.checked == true && engChars.checked == true) {
+            generateChar(25, engLowerLang);
+        } else if (lowerChars.checked == true && rusChars.checked == true) {
+            generateChar(32, rusLowerLang);
+        } else if (specialChars.checked == true) {
+            generateChar(18, otherCharacters);
+        } else if (numbers.checked == true) {
+            generateChar(10, numberCharacters);
         }
-
-        function generateEngUpperChar() {
-            let random = Math.floor(Math.random() * 25);
-            resultChar = engUpperLang[random];
+      }
+    
+      function generatePassword() {
+        let length = passwordLength.value;
+        let password = "";
+        for (let i = 0; i < length; i++) {
+          checkSettings();
+          password += resultChar;
         }
+        resultBlock.classList.add("show");
+        passwordResult.innerText = password;
+        passwordResult.style.animation = "type 5s steps(50, end) 1";
+      }
 
-        function generateRusUpperChar() {
-            let random = Math.floor(Math.random() * 32);
-            resultChar = rusUpperLang[random];
-        }
-
-        function generateRusLowerChar() {
-            let random = Math.floor(Math.random() * 32);
-            resultChar = rusLowerLang[random];
-        }
-
-        function generateNumbersAndOtherChar() {
-            let characters = otherCharacters + numberCharacters;
-            let random = Math.floor(Math.random() * 18);
-            resultChar = characters[random];
-        }
-
-        function generateEngUpperAndNumbers() {
-            let characters = engUpperLang + numberCharacters;
-            let random = Math.floor(Math.random() * 35);
-            resultChar = characters[random];
-        }
-
-        function generateEngLowerAndNumbers() {
-            let characters = engLowerLang + numberCharacters;
-            let random = Math.floor(Math.random() * 35);
-            resultChar = characters[random];
-        }
-
-        function generateRusUpperAndNumbers() {
-            let characters = rusUpperLang + numberCharacters;
-            let random = Math.floor(Math.random() * 42);
-            resultChar = characters[random];
-        }
-
-        function generateRusLowerAndNumbers() {
-            let characters = rusLowerLang + numberCharacters;
-            let random = Math.floor(Math.random() * 42);
-            resultChar = characters[random];
-        }
-
-        function generateEngUpperAndSpecialChars() {
-            let characters = engUpperLang + otherCharacters;
-            let random = Math.floor(Math.random() * 33);
-            resultChar = characters[random];
-        }
-
-        function generateEngLowerAndSpecialChars() {
-            let characters = engLowerLang + otherCharacters;
-            let random = Math.floor(Math.random() * 33);
-            resultChar = characters[random];
-        }
-
-        function generateRusUpperAndSpecialChars() {
-            let characters = rusUpperLang + otherCharacters;
-            let random = Math.floor(Math.random() * 40);
-            resultChar = characters[random];
-        }
-
-        function generateRusLowerAndSpecialChars() {
-            let characters = rusLowerLang + otherCharacters;
-            let random = Math.floor(Math.random() * 40);
-            resultChar = characters[random];
-        }
-
-        function checkSettings() {
-            if (upperChars.checked == true && engChars.checked == true && numbers.checked == true) {
-                generateEngUpperAndNumbers();
-            } else if (lowerChars.checked == true && engChars.checked == true && numbers.checked == true) {
-                generateEngLowerAndNumbers();
-            } else if (lowerChars.checked == true && rusChars.checked == true && numbers.checked == true) {
-                generateRusLowerAndNumbers();
-            } else if (upperChars.checked == true && rusChars.checked == true && numbers.checked == true) {
-                generateRusUpperAndNumbers();
-            } else if (upperChars.checked == true && engChars.checked == true && specialChars.checked == true) {
-                generateEngUpperAndSpecialChars();
-            } else if (lowerChars.checked == true && engChars.checked == true && specialChars.checked == true) {
-                generateEngLowerAndSpecialChars();
-            } else if (lowerChars.checked == true && rusChars.checked == true && specialChars.checked == true) {
-                generateRusLowerAndSpecialChars();
-            } else if (upperChars.checked == true && rusChars.checked == true && specialChars.checked == true) {
-                generateRusUpperAndSpecialChars();
-            } else
-            if (specialChars.checked == true && numbers.checked == true) { 
-                generateNumbersAndOtherChar();
-            } else if (upperChars.checked == true && engChars.checked == true) {
-                generateEngUpperChar();
-            } else if (upperChars.checked == true && rusChars.checked == true) {
-                generateRusUpperChar();
-            } else if (lowerChars.checked == true && engChars.checked == true) {
-                generateEngLowerChar();
-            } else if (lowerChars.checked == true && rusChars.checked == true) {
-                generateRusLowerChar();
-            } else if (specialChars.checked == true) {
-                generateOtherChar();
-            } else if (numbers.checked == true) {
-                generateNumber();
-            }
-        }
-
-        function generatePassword() {
-            let length = passwordLength.value;
-            let password = "";
-            for (let i = 0; i < length; i++) {
-                checkSettings();
-                password += resultChar;
-            }
-            resultBlock.classList.add("show");
-            passwordResult.innerText = password;
-        }
-        generatePassword();
-
+      generatePassword();
     }
 }
