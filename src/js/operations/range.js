0cf6036ddef7455e725dfc1f114b02d9ef0389ea
Artiom Kochkin <112868679+ArtiomKochkin@users.noreply.github.com>
2023-08-15 16:16:44 +0700

    Delete src directory

--- src/js/operations/range.js
+++
@@ -1,8 +0,0 @@
-export function range() {
-    let rangeSettings = document.getElementById("rangeSettings");
-    let rangeText = document.getElementById("rangeText");
-
-    rangeSettings.oninput = function() {
-        rangeText.value = rangeSettings.value;
-    }
-}
